a217990cd83af2076608ccfff853911a1ca80269
Umair Abbasi <umair.abbasi@fourfridays.com>
2017-09-21 15:37:20 +0300

    added lightbox support with full jquery
    Needed the complete jquery instead of the slim version for lightbox. Add some weight to the page.

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             dist: {
-                src: ['node_modules/jquery/dist/jquery.slim.js', 'node_modules/bootstrap/js/dist/util.js', 'node_modules/bootstrap/js/dist/collapse.js', 'src/flinthillsparanormal.js'],
+                src: ['node_modules/jquery/dist/jquery.min.js', 'node_modules/bootstrap/js/dist/util.js', 'node_modules/bootstrap/js/dist/collapse.js', 'node_modules/lightbox2/dist/js/lightbox.js', 'src/flinthillsparanormal.js'],
                 dest: 'dist/js/flinthillsparanormal.js'
             }
         },
