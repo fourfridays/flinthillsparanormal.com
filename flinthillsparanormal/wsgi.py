5c740eb8e464051bd174765a900d9609c73ae76b
Umair Abbasi <umair.abbasi@fourfridays.com>
2017-09-21 13:25:38 +0000

    modified gruntfile path for production

--- flinthillsparanormal/wsgi.py
+++ flinthillsparanormal/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flinthillsparanormal.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flinthillsparanormal.settings.production")
 
 application = get_wsgi_application()
