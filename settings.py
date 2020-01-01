8fcaddb8da9ab87532862fe2273c6366bb109999
Umair Abbasi <umair.abbasi@fourfridays.com>
2019-12-31 22:24:34 -0800

    added default str for env captcha setting

--- settings.py
+++ settings.py
@@ -56,5 +56,5 @@ INSTALLED_APPS.extend([
     'wagtail.contrib.table_block',
 ])
 
-RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY')
-RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY')
+RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY', default='')
+RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY', default='')
