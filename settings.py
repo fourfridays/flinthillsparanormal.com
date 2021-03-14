46acb3739542f712b9640baf6968f0c8934d83b3
Umair Abbasi <umair.abbasi@fourfridays.com>
2021-03-14 11:50:13 -0700

    replaced aldryn_wagtail
    Custom installation of Wagtail to override admin templates.

--- settings.py
+++ settings.py
@@ -52,7 +52,31 @@ INSTALLED_APPS.extend([
     'wagtailcaptcha',
     
     'wagtail.contrib.table_block',
+
+    'wagtail.contrib.forms',
+    'wagtail.contrib.redirects',
+    'wagtail.contrib.modeladmin',
+    'wagtail.embeds',
+    'wagtail.sites',
+    'wagtail.users',
+    'wagtail.snippets',
+    'wagtail.documents',
+    'wagtail.images',
+    'wagtail.search',
+    'wagtail.admin',
+    'wagtail.core',
+
+    'modelcluster',
+    'taggit',
 ])
+
+MIDDLEWARE.extend([
+    # add your own middlewares here
+    'wagtail.contrib.legacy.sitemiddleware.SiteMiddleware',
+    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
+])
+
+WAGTAIL_SITE_NAME = 'Flint Hills Paranormal'
 
 RECAPTCHA_PUBLIC_KEY = os.getenv('RECAPTCHA_PUBLIC_KEY', default='')
 RECAPTCHA_PRIVATE_KEY = os.getenv('RECAPTCHA_PRIVATE_KEY', default='')
